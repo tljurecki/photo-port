440ba7f188f5d71a7e0f63356ef18beb3f3ed7b1
Tanya Jurecki <tljurecki@gmail.com>
2022-07-08 13:06:43 -0400

    added validation to contact form

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
   ]); 
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
   
   return (
     <div>
@@ -24,11 +25,18 @@ function App() {
       categories={categories}
       setCurrentCategory={setCurrentCategory}
       currentCategory={currentCategory}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
       ></Nav>
      <main>
-      <ContactForm />
+      {!contactSelected ? (
+        <>
       <Gallery currentCategory={currentCategory}></Gallery> 
       <About />
+      </>
+      ) : (
+        <ContactForm />
+      )}
      </main>
     </div>
   );
